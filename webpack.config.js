db356503f3ca76648e45f80ce2016337c5844279
ngoctv <tongoc92@gmail.com>
2017-05-11 11:29:34 +0700

    -webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var BUNDLE_PATH = path.resolve(__dirname, "public");
+const APP_DIR   = path.resolve(__dirname, 'client');
+
+var config = {
+    entry : './client/app/index.jsx',
+    output : {
+        filename : 'bundle.js',
+        path : BUNDLE_PATH
+    },
+    module : {
+        loaders: [
+            {
+                test   : /\.jsx?/,
+                include: APP_DIR,
+                loader : 'babel-loader',
+                exclude: /node_modules/,
+                query  : {compact: false},
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    }
+};
+module.exports = config;
+
+
