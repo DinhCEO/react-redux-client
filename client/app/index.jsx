eeee8bc84b2b5d7f8dd92e4171dcf6eb119ae57f
ngoctv <tongoc92@gmail.com>
2017-05-12 17:16:23 +0700

    - redux-thunk

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import reducer from './reducers/index.jsx'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+
 import {Router, browserHistory} from 'react-router'
 import {Provider} from 'react-redux'
 import routers from './routers.jsx'
 
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 console.log(store.getState());
 
 ReactDOM.render(
